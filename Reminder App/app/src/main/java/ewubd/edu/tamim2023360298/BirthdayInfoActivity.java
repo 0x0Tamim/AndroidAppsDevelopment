1c3c6398b7b36528a63c36e14dd2ebe0b1b7ed81
0x0Tamim <mmtamimuzzaman@gmail.com>
2026-01-26 03:03:40 +0600

    Designed singnUp activity ui and fixed errors

---
+++ Reminder App/app/src/main/java/ewubd/edu/tamim2023360298/BirthdayInfoActivity.java
@@ -0,0 +1,15 @@
+package ewubd.edu.tamim2023360298;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BirthdayInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthday_info);
+    }
+}
