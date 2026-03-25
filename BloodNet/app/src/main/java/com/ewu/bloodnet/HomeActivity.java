fcbaaaebf03af117cb45250d6fc68f57f5c76564
0x0Tamim <mmtamimuzzaman@gmail.com>
2026-03-25 22:11:18 +0600

    HomeActivity done

--- BloodNet/app/src/main/java/com/ewu/bloodnet/HomeActivity.java
+++ BloodNet/app/src/main/java/com/ewu/bloodnet/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.ewu.bloodnet;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.content.Intent;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,31 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        LinearLayout cardProfile = findViewById(R.id.cardProfile);
+
+        cardProfile.setOnClickListener(view -> {
+
+            Toast.makeText(HomeActivity.this, "Profile Clicked", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(intent);
+            finish();
         });
+
+
+        LinearLayout cardSearch = findViewById(R.id.cardSearch);
+
+        cardSearch.setOnClickListener(view -> {
+
+            Toast.makeText(HomeActivity.this, "Search Clicked", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+
+
     }
 }
